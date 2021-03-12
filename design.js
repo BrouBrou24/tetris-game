53245b12f2ee92e3930671aae1b37f1fd85cc114
Michel Brouillette <mitch.brou@hotmail.com>
2021-03-12 15:46:36 -0700

    change board creation

--- design.js
+++ design.js
@@ -2,13 +2,11 @@ const SQ = 20;
 const EMPTY = "#FFF";
 let board = [ ];
 var canvas = document.querySelector("#canvas")
-function makeGrid() {
-    c = 0;
-    while (c < 21) {
-        var newRow = canvas.insertRow(c)
-        c += 1;
-        for (let r = 0; r < 11; r++)
-            newRow.insertCell(r);
+
+for ( let r = 0; r < 20; r++){
+      board[r] = [ ];
+      for( let c = 0; c < 10; c++){
+        board[r][c] = EMPTY;
     }
 }
 
